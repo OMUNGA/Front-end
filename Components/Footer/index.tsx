bab84c7fd02f1cb5f54d6970c8707647e62a3edc
Pedro Franco <pedrofrancodev@gmail.com>
2022-09-01 09:02:06 +0100

    feat: Replacing the link tag with the anchor

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import { Container } from "../../styles/Container"
 import { FooterSection, FirstColumn, SecondColumn, Children1, Children2, Children3, SearchBox, ParentDiv, Gradient, Logo} from "./styles"
-import Link from "next/Link"
+// import Link from "next/Link"
 import Input from "../Input"
 
 export const Footer: React.FC =()=> {
@@ -16,20 +16,20 @@ export const Footer: React.FC =()=> {
                         <Children1>
                             <h3>Home</h3>
                             <ul>
-                                <li><Link href="#">Artigos</Link></li>
-                                <li><Link href="#">Forum</Link></li>
+                                <li><a href="#">Artigos</a></li>
+                                <li><a href="#">Forum</a></li>
                             </ul>
                         </Children1>
                         <Children2>
                             <h3>Sobre</h3>
                             <ul>
-                                <li><Link href="#">Quem somos</Link></li>
-                                <li><Link href="#">O projeto Omunga</Link></li>
+                                <li><a href="#">Quem somos</a></li>
+                                <li><a href="#">O projeto Omunga</a></li>
                             </ul>
                         </Children2>
                         <Children3>
                             <h3>Contactos</h3>
-                            <Link href="mailto: faleconosco@omunga.com" target="_top">faleconosco@omunga.com</Link>
+                            <a href="mailto: faleconosco@omunga.com" target="_top">faleconosco@omunga.com</a>
                         </Children3>
                     </SecondColumn>
                 </ParentDiv>
