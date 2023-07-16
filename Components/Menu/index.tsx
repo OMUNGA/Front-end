e65936be8b4916bd0e0007b6024057d3c46bd978
FelixPereira <felixpereira538@gmail.com>
2023-07-16 17:46:17 +0100

    refactor: avoid repeating Link component by creating an array of links and use map for rendering them

--- Components/Menu/index.tsx
+++ Components/Menu/index.tsx
@@ -1,41 +1,56 @@
-import { Container } from "../../styles/Container"
-import { MenuBar, Image, Nav, Flex1, Flex2, Flex3, Avatar, Logo } from "./styles"
-import { FaAngleRight } from "react-icons/fa"
-import Button from "../Button"
-import { useAuth } from "../../context/AppContext"
-import { MenuMobile } from "./MenuMobile"
-import Link from "next/link"
+import { Container } from '../../styles/Container';
+import {
+  MenuBar,
+  Image,
+  Nav,
+  Flex1,
+  Flex2,
+  Flex3,
+  Avatar,
+  Logo,
+} from './styles';
+import { FaAngleRight } from 'react-icons/fa';
+import Button from '../Button';
+import { useAuth } from '../../context/AppContext';
+import { MenuMobile } from './MenuMobile';
+import Link from 'next/link';
 export default function Menu() {
+  const { user, logout } = useAuth();
 
-    const { user, logout } = useAuth()
-    return (
-        <>
-            <MenuBar>
-                <Container display="">
-                    <Nav>
-                        <Flex1>
-                            <Logo>Omunga</Logo>
-                        </Flex1>
-                        <Flex2>
-                            <Link href="./">Home</Link>
-                            <Link href="./Articles">Artigos</Link>
-                            <Link href="./Foruns">Forum</Link>
-                            <Link href="#">Sobre</Link>
-                        </Flex2>
-                            {
-                                user ?
-                                    <Avatar src={user?.image} onClick={() => logout()} />
-                                : null
-                            }
-                        <Flex3>
-                            <a href="./login">
-                                <Button Text="Entrar" Icon={<FaAngleRight />} />
-                            </a>
-                        </Flex3>
-                    </Nav>
-                </Container>
-            </MenuBar>
-            <MenuMobile/>
-        </>
-    )
+  const menuLinks = [
+    { href: '/', label: 'Home' },
+    { href: '/artigos', label: 'Artigos' },
+    { href: '/forum', label: 'Fórum' },
+    { href: '/sobre', label: 'Sobre' },
+  ];
+
+  return (
+    <>
+      <MenuBar>
+        <Container display=''>
+          <Nav>
+            <Flex1>
+              <Logo>Omunga</Logo>
+            </Flex1>
+            <Flex2>
+              {menuLinks.map((link) => (
+                <Link key={link.label} href={link.href}>
+                  {link.label}
+                </Link>
+              ))}
+            </Flex2>
+            {user ? (
+              <Avatar src={user?.image} onClick={() => logout()} />
+            ) : null}
+            <Flex3>
+              <a href='./login'>
+                <Button Text='Entrar' Icon={<FaAngleRight />} />
+              </a>
+            </Flex3>
+          </Nav>
+        </Container>
+      </MenuBar>
+      <MenuMobile />
+    </>
+  );
 }
