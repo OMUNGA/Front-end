56363f33999c44c20d20661dfecc600314e1e2c6
FelixPereira <felixpereira538@gmail.com>
2023-07-16 18:33:20 +0100

    refactor: rename url's slug from Articles to artigos

---
+++ pages/artigos/styles.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const PaginationSection = styled.div`
+    
+`
+
+export const ButtonControlsBox = styled.div`
+    background: ${({theme})=> theme.colors.standard.white};
+    box-shadow: 4px 4px 10px rgba(0,0,0,0.1);
+    height: 20px;
+    width: 40px;
+`
