fe932339f799e23aff6575372ca9011c88fffd87
FelixPereira <felixpereira538@gmail.com>
2023-07-16 16:59:01 +0100

    refactor: convert CardBody component into a link for the article's page

--- Components/Article/index.tsx
+++ Components/Article/index.tsx
@@ -1,41 +1,39 @@
-import ptBR from 'date-fns/locale/pt-BR'
-import { format } from 'date-fns'
-import { BiLike } from 'react-icons/bi'
-import { FaRegCommentDots} from 'react-icons/fa'
-import {ArticleCard, CardHeader, CardBody, CardFooter} from "./styles"
+import ptBR from 'date-fns/locale/pt-BR';
+import { format } from 'date-fns';
+import { BiLike } from 'react-icons/bi';
+import { FaRegCommentDots } from 'react-icons/fa';
+import { ArticleCard, CardHeader, CardBody, CardFooter } from './styles';
 
 interface SingleArticleProps {
-    article: any;
+  article: any;
 }
 
-export const Article:React.FC<SingleArticleProps> = ({article})=> {
-    return(
-        <ArticleCard>
-            <CardHeader>
-                <img src={article.avatarURL} alt='avatar' />
-                <small>
-                    <strong>{article.author}</strong>
-                    <span>
-                        {  
-                            format(article.availableAt, "d '  ' MMMM '  ' yyyy", {
-                                locale: ptBR
-                            })
-                        }
-                    </span>
-                </small>
-            </CardHeader>
-            <CardBody>
-                <h3>{article.title}</h3>
-                <img src={article.articleImg} alt='Article-img' />
-            </CardBody>
-            <CardFooter>
-                    <button>
-                        <BiLike />
-                    </button>
-                    <button>
-                        <FaRegCommentDots />
-                    </button>
-            </CardFooter>
-        </ArticleCard>
-    )
-}
+export const Article: React.FC<SingleArticleProps> = ({ article }) => {
+  return (
+    <ArticleCard>
+      <CardHeader>
+        <img src={article.avatarURL} alt='avatar' />
+        <small>
+          <strong>{article.author}</strong>
+          <span>
+            {format(article.availableAt, "d '  ' MMMM '  ' yyyy", {
+              locale: ptBR,
+            })}
+          </span>
+        </small>
+      </CardHeader>
+      <CardBody href={(article.id).toString()}>
+        <h3>{article.title}</h3>
+        <img src={article.articleImg} alt='Article-img' />
+      </CardBody>
+      <CardFooter>
+        <button>
+          <BiLike />
+        </button>
+        <button>
+          <FaRegCommentDots />
+        </button>
+      </CardFooter>
+    </ArticleCard>
+  );
+};
