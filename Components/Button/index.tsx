3df11ef68e9236c4d7ded988b2fe9f6a2273b493
FelixPereira <felixpereira538@gmail.com>
2023-07-16 18:20:55 +0100

    refactor: set type as an optional prop

--- Components/Button/index.tsx
+++ Components/Button/index.tsx
@@ -1,15 +1,15 @@
-import {StyledButton} from "./styles"
+import { StyledButton } from './styles';
 
 interface ButtonPropos {
-    Text: string,
-    Icon: any,
-    type: string
+  Text: string;
+  Icon: any;
+  type?: string;
 }
 
-export default function Button({Text, Icon}: ButtonPropos) {
-    return(
-        <StyledButton buttonWidth={Icon? "130px": ""}>
-           {Text} {Icon}
-        </StyledButton>
-    )
+export default function Button({ Text, Icon }: ButtonPropos) {
+  return (
+    <StyledButton buttonWidth={Icon ? '130px' : ''}>
+      {Text} {Icon}
+    </StyledButton>
+  );
 }
